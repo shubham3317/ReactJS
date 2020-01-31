06c19d48b36f4d4dc2db6b0beaa9f442c066632a
Shubham Shinde <shubhamshinde266@gmail.com>
2020-01-31 18:57:22 +0530

    "Check Commit"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import MainApp from "./components/MainApp";
 import Counter from "./components/Counter";
-
+//Simple CRUD Example
 class App extends Component {
   render() {
     return (
